7bfe0e346a96865acd66254e1efbf4422987262c
Lucas <lucasdcifuentes@gmail.com>
2020-08-30 21:22:21 -0500

    add agregar-trabajos to privates routes

--- src/Routes/private.routes.js
+++ src/Routes/private.routes.js
@@ -3,11 +3,13 @@ import { Switch } from "react-router-dom";
 import { PrivateRoute } from "./helperRoutes";
 
 import Admin from "../Pages/Admin";
+import AgregarTrabajos from "../Pages/AgregarTrabajos";
 
 const PrivateRoutes = () => {
   return (
     <Switch>
       <PrivateRoute exact path="/admin" component={Admin} />
+      <PrivateRoute exact path="/agregar-trabajo" component={AgregarTrabajos} />
     </Switch>
   );
 };
