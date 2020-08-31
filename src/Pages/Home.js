e31f76fb7d84034ffa66fefb2b8af6e173fc5f44
Lucas <lucasdcifuentes@gmail.com>
2020-08-30 21:19:47 -0500

    Emilinate carousel

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,14 +4,6 @@ import "../Styles/Home.css";
 
 import Card from "../Components/Card.js";
 
-import Carousel from "../Components/Carousel";
-
-//imagenes carousel
-import One from "../Assets/Imagenes/PatioCasaArboles.jpeg";
-import Two from "../Assets/Imagenes/MedianeraMadera.jpeg";
-import Third from "../Assets/Imagenes/PiletaMedianeraMadera.jpeg";
-import Four from "../Assets/Imagenes/PatioCasaAmarilla.jpeg";
-
 //imagenes card
 import TrabajosImg from "../Assets/Imagenes/PatioCasaArboles.jpeg";
 import ServiciosImg from "../Assets/Imagenes/PatioQuinta.jpeg";
@@ -19,9 +11,9 @@ import NosotrosImg from "../Assets/Imagenes/PiletaPisoMaderaLejos.jpeg";
 
 const Home = () => {
   return (
-    <div className="home">
-      <Carousel one={One} two={Two} third={Third} four={Four} />
+    <div className="home center">
       <div className="container">
+        <h1>Inicio</h1>
         <div className="row">
           <div className="col">
             <Card
