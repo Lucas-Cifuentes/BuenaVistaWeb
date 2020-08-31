7f34d61210b686b507f5ae54fc50a81f46ba0701
Lucas <lucasdcifuentes@gmail.com>
2020-08-30 21:16:33 -0500

    change admin page

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "../Styles/Admin.css";
+
 import "firebase/auth";
 import { useFirebaseApp, useUser } from "reactfire";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 
 const Admin = () => {
   const firebase = useFirebaseApp();
@@ -12,20 +14,36 @@ const Admin = () => {
     firebase
       .auth()
       .signOut()
-      .then(() => <Redirect to="/login" />)
+      .then(() => <Redirect to="/" />)
       .catch(() => console.log("Ha ocurrido un error"));
   };
 
   return (
-    <div className="admin">
-      <h3>Hola {user.email}</h3>
-      <button
-        className="btn waves-effect waves-light green lighten-1"
-        type="button"
-        onClick={SingOut}
-      >
-        Cerrar Sesion
-      </button>
+    <div className="container center">
+      <div className="admin">
+        <div className="row">
+          <h3>Sesion Iniciada con el Email:</h3>
+          <h3>{user.email}</h3>
+          <div className="col s6">
+            <button
+              className="btn waves-effect waves-light red"
+              type="button"
+              onClick={SingOut}
+            >
+              Cerrar Sesion
+            </button>
+          </div>
+          <div className="col s6">
+            <Link
+              to="agregar-trabajo"
+              type="button"
+              className="btn waves-effect waves-light green lighten-1"
+            >
+              Añadir un Trabajo
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
